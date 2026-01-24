67f1e6aaefa5212afb2f3c28b69e92e45bc16407
zaidprd <160503080+zaidprd@users.noreply.github.com>
2026-01-25 04:07:19 +0700

    sudah

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,22 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare';
-import sitemap from '@astrojs/sitemap'; // Tambahkan ini agar mudah diindex Google
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  // URL utama agar sitemap.xml Anda tidak error
   site: 'https://picks.zaidly.com',
   output: 'server',
   adapter: cloudflare({
-    imageService: 'compile' // Perbaikan untuk pesan error Sharp tadi
+    // Matikan warning SESSION KV jika Anda hanya membuat blog
+    runtime: { mode: 'off' } 
   }),
+  // Solusi wajib agar Cloudflare tidak protes soal Sharp
+  image: {
+    service: { entrypoint: 'astro/assets/services/sharp' }
+  },
   integrations: [
     tailwind(),
-    sitemap() // Wajib agar Google tahu daftar artikel Anda
+    sitemap() // Bot Google sangat butuh ini untuk indexing
   ],
 });
